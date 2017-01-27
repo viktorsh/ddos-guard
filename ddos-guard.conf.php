2f2b44840a866b0999e7a368bf5a67b962619989
Viktor <shiryakovv@gmail.com>
2017-01-27 16:14:27 +0700

    Добавлено todo.txt Прокомментирован уонфиг

--- ddos-guard.conf.php
+++ ddos-guard.conf.php
@@ -1,17 +1,15 @@
 <?php
-
 return [
-    'logFilePath' => '/var/log/ddos-guard.log',
-    'estimate' => 500,
-    'disabled'  => true,
-    'logs' => [
+    'logFilePath' => '/var/log/ddos-guard.log',         // *Файл лога работы скрипта
+    'estimate' => 500,                                  // *время в секундах на которое блокируется ip адрес
+    'disabled'  => true,                                // *Запрет блокировки ip адресов, только пищет в лог и в файл ddos-guard.status.dat
+    'logs' => [                                         // Масссив обработчиков логов
         'alsi.kz' => [
-            'filePath' => '/var/log/alsi_access_log-20161225',
-            'limit' => [[100, 180]], // 100 раз за 180 секунд
-            //'htaccess'=> __DIR__ . '/.htaccess',
-            'useIptables'=> true,
-            'rowHandler' => 'row_handler_default'
-
+            'filePath' => '/var/log/alsi_access_log-20161225',          // * Путь до файла логов для парсинга и анализа
+            'limit' => [[100, 180]],                                    // * массиа лимиты блокировок. первый количество записей в логи, второй за какое время
+            //'htaccess'=> __DIR__ . '/.htaccess',                      // путь к htaacess файлу в котором будем блокировать ip через deny from
+            'useIptables'=> true,                                       // Флаг использования файрвола iptables для блокировки ip по порту
+            'rowHandler' => 'row_handler_default'                       // * имя файла в каталоге handlers, который парсит строку из файла логов
         ]
     ]
 ];
